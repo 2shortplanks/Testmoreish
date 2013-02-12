5d23b3bc131d649d68f62871b2f0c776044ab72c
Mark Fowler <mark@twoshortplanks.com>
2013-02-12 13:33:05 -0500

    build selftest.html automatically too

--- build/makereadme.pl
+++ build/makereadme.pl
@@ -10,11 +10,41 @@ use File::Spec::Functions qw(:ALL);
 # note everything ascii, so don't worry about utf8 et al
 open my $in, "<", catfile($FindBin::Bin,updir, "testmorish.js");
 open my $out, ">", catfile($FindBin::Bin,updir, "README.txt");
+open my $selftest, ">", catfile($FindBin::Bin,updir, "selftest.html");
 
+print $selftest <<'HTML';
+<html>
+
+<!-- this file is automatically created from testmoreis.js DO NOT UPDATE -->
+
+<head>
+<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+<script src="testmorish.js"></script>
+</head>
+<body>
+
+<h1>Self-test Suite for Testmorish</h1>
+
+<script>
+HTML
+
+my $example = undef;
 while (<$in>) {
 	last unless s{\A// ?}{}s;
+
 	print $out $_;
+
+	s/  //;
+	print $selftest $_ if $example;
+	$example = 1 if /Example usage/;
 }
+
+print $selftest <<'HTML';
+</script>
+
+</body>
+</html>
+HTML
 
 =head1 NAME
 
